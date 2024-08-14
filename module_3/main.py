3c720439ac8d898b08158f8b3f728ae58bc47c16
Ponedelnik <satewriter128@gmail.com>
2024-08-14 21:02:05 +0300

    .

---
+++ module_3/main.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words):
+  same_words = []
+  for i in other_words:
+    if root_word.lower() in i.lower() or i.lower() in root_word.lower():
+      same_words.append(i)
+  print(f' Корень: {root_word} \n Список слов: {other_words}')
+  return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+print(f'Однокоренные: {result1}')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(f'Однокоренные: {result2}')
